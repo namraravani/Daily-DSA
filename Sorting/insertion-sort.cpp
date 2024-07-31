a484e8269339d846b7cc970889f550f73648e05b
namraravani <namraravani8@gmail.com>
2024-07-31 13:12:46 +0530

    insertion-sort-commit

---
+++ Sorting/insertion-sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {5, 4, 3, 2, 1};
+
+    for (int i = 1; i < 5; i++)
+    {
+        int curr = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > curr)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = curr;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i];
+    }
+    return 0;
+}
