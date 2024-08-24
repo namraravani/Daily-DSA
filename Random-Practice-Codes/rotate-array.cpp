bafb5d7ebf8f50099435568bd2da9d0bdbf2920f
namraravani <namraravani8@gmail.com>
2024-08-24 23:16:22 +0530

    rotate-array

---
+++ Random-Practice-Codes/rotate-array.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<int> &nums, int k)
+{
+
+    k = k % nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+}
+int main()
+{
+    return 0;
+}
