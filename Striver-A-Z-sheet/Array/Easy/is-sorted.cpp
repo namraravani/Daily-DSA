307a4464a6c0f34b7367de3837d08ac2e70475d8
namraravani <namraravani8@gmail.com>
2024-10-08 23:07:53 +0530

    is-sorted-leetcode

---
+++ Striver-A-Z-sheet/Array/Easy/is-sorted.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(vector<int> &nums)
+{
+    int n = nums.size();
+    int cnt = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i - 1] > nums[i])
+        {
+            cnt++;
+        }
+    }
+    if (nums[n - 1] > nums[0])
+    {
+        cnt++;
+    }
+    return cnt <= 1;
+}
+
+int main()
+{
+    return 0;
+}
