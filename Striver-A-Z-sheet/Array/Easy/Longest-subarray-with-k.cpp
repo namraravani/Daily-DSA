e20b3dcefbc085636315997551b4876dd2888368
namraravani <namraravani8@gmail.com>
2024-10-17 23:02:50 +0530

    longest-subarray-with-k

---
+++ Striver-A-Z-sheet/Array/Easy/Longest-subarray-with-k.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lenOfLongSubarr(int A[], int N, int K)
+{
+
+    unordered_map<int, int> mp;
+    int maxLen = 0;
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += A[i];
+        if (sum == K)
+        {
+            maxLen = max(maxLen, i + 1);
+        }
+        if (mp.find(sum - K) != mp.end())
+        {
+            maxLen = max(maxLen, i - mp[sum - K]);
+        }
+        if (mp.find(sum) == mp.end())
+        {
+            mp[sum] = i;
+        }
+    }
+    return maxLen;
+}
+
+int main()
+{
+    return 0;
+}
