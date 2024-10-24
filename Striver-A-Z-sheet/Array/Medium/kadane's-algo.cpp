12e103fac6fa6a0afaed02ca72f719c29342ae4b
namraravani <namraravani8@gmail.com>
2024-10-24 23:25:12 +0530

    kadane's algo

---
+++ Striver-A-Z-sheet/Array/Medium/kadane's-algo.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubArray(vector<int> &nums)
+{
+
+    int maxi = INT_MIN;
+    int sum = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum += nums[i];
+
+        if (sum > maxi)
+        {
+            maxi = sum;
+        }
+
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+
+    return maxi;
+}
+
+int main()
+{
+    return 0;
+}
